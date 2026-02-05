960ef6009c523bdc9a4ade7f13de563ca72738f6
sudeephb <sudeephb19@gmail.com>
2026-02-05 16:13:30 +0545

    Apply patch for ec snapshot fix

--- internal/rbd/controllerserver.go
+++ internal/rbd/controllerserver.go
@@ -1328,6 +1328,8 @@ func (cs *ControllerServer) doSnapshotClone(
 	f := []string{librbd.FeatureNameLayering, librbd.FeatureNameDeepFlatten}
 	cloneRbd.ImageFeatureSet = librbd.FeatureSetFromNames(f)
 
+	cloneRbd.DataPool = parentVol.DataPool
+
 	err := cloneRbd.Connect(cr)
 	if err != nil {
 		return cloneRbd, err
